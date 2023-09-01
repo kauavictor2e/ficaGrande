53e9b9255a4464e45b7474d0b3f4d8f3216dd901
kauavictor2e <132069230+kauavictor2e@users.noreply.github.com>
2023-09-01 09:36:11 -0300

    primeiro paciente

---
+++ 2e-3trim-nutricao-main/js/principal.js
@@ -0,0 +1,11 @@
+//atrubui o valor da tag h1 para a variável titulo
+let titulo = document.querySelector("h1");
+//motra no console do navegador a variavel título
+console.log(titulo);
+//modi)fica o valor de titulo para frase "Fic grandão birlllll"
+titulo.textContent = "Fica grandão birlllll";
+
+//------aqui vamos mexer na tabela de imc------
+
+let paciente = document.querySelector("#primeiro-paciente");
+console.log(paciente);
